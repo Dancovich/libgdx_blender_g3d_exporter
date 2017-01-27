7c3ac63fab9b565bb860495a2a42835b61c03760
Danilo Costa Viana <dcviana@gmail.com>
2017-01-27 15:34:42 -0300

    Preparando lançamento da versão 0.2.6

--- io_scene_g3d/__init__.py
+++ io_scene_g3d/__init__.py
@@ -31,7 +31,7 @@ from .g3d_exporter import G3DJExporterOperator, G3DBExporterOperator
 bl_info = {
     "name": "LibGDX G3D Exporter",
     "author": "Danilo Costa Viana",
-    "version": (0, 2, 5),
+    "version": (0, 2, 6),
     "blender": (2, 76, 0),
     "location": "File > Import-Export",
     "description": "Export scene to LibGDX format",
